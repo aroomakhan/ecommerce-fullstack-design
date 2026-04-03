0b32d413b9d016c0316899ba1d428b60a226ece3
Muteeba Khan <misskhan0273@gmail.com>
2026-04-03 06:25:37 -0700

    Update CORS origin for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -1,39 +1,4 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// require('dotenv').config();
 
-// // 1. Import Routes
-// const productRoutes = require('./routes/productRoutes');
-// const authRoutes = require('./routes/authRoutes'); 
-
-// const app = express();
-
-// // 2. Middleware (Must come BEFORE routes)
-// app.use(cors());
-// app.use(express.json()); 
-
-// // 3. Use Routes
-// app.use('/api/products', productRoutes);
-// app.use('/api/auth', authRoutes); // Moved this up here
-
-// // 4. Test Route
-// app.get('/', (req, res) => {
-//   res.send("The API is running...");
-// });
-
-// // 5. Database Connection
-// const MONGO_URI = process.env.MONGO_URI;
-// mongoose.connect(MONGO_URI)
-//   .then(() => console.log("✅ MongoDB Connected Successfully!"))
-//   .catch((err) => console.log("❌ MongoDB Connection Error:", err));
-
-// // 6. Start the Server (Always at the very bottom)
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server is listening on http://localhost:${PORT}`);
-// });
-// module.exports = app;
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -48,7 +13,7 @@ const app = express();
 // 2. Middleware 
 // UPDATED: Added specific options to ensure Vercel/Browsers play nice
 app.use(cors({
-  origin: "*",
+  origin: ["https://ecommerce-fullstack-design-drab.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
@@ -65,10 +30,16 @@ app.get('/', (req, res) => {
 
 // 5. Database Connection
 const MONGO_URI = process.env.MONGO_URI;
+// 6. Start the Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is listening on http://localhost:${PORT}`);
+});
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected Successfully!"))
   .catch((err) => console.log("❌ MongoDB Connection Error:", err));
+
 
 
 
